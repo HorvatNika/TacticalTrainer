884eb6a6e275a6cbdabae185bb1b1068fbd0fa50
HorvatNika <nhorvat1@student.unipu.hr>
2024-08-12 23:26:40 +0200

    ...Zašto mi opet ne radi ono kaj želim da radi....?  :)))))

--- src/views/ScoreRank.vue
+++ src/views/ScoreRank.vue
@@ -1,231 +1,345 @@
 <template>
-    <div class="score-rank">
-      <h2 class="header">Score List</h2>
-  
-      <!-- Filteri -->
-      <div class="filters">
-        <label>
-          <input type="radio" v-model="filterCategory" value="all" /> All
-        </label>
-        <label>
-          <input type="radio" v-model="filterCategory" value="military-library" /> Military Library
-        </label>
-      </div>
-  
-      <!-- Tablice -->
-      <div v-if="filterCategory === 'all'" class="table-section">
-        <h3 class="table-title">Overall Scores</h3>
-        <table class="score-table main-table">
-          <thead>
-            <tr class="table-header">
-              <th>Rank</th>
-              <th>User</th>
-              <th>Total Score</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-  
-      <div v-else-if="filterCategory === 'military-library'" class="table-section">
-        <div class="table-pair">
-          <div class="table-container">
-            <h3 class="table-title">Military Encyclopedia Library</h3>
-            <table class="score-table main-table">
-              <thead>
-                <tr class="table-header">
-                  <th>Rank</th>
-                  <th>User</th>
-                  <th>Total Score</th>
-                </tr>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-  
-            <div class="filters library-filters">
-              <label>
-                <input type="radio" v-model="libraryFilterCategory" value="weapons" /> Weapons Encyclopedia
-              </label>
-              <label>
-                <input type="radio" v-model="libraryFilterCategory" value="vehicles" /> Vehicle Encyclopedia
-              </label>
-            </div>
-  
-            <div v-if="libraryFilterCategory === 'weapons'" class="table-section">
-              <h3 class="table-title">Weapons Encyclopedia</h3>
-              <table class="score-table main-table">
-                <thead>
-                  <tr class="table-header">
-                    <th>Rank</th>
-                    <th>User</th>
-                    <th>Total Score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-            </div>
-  
-            <div v-if="libraryFilterCategory === 'vehicles'" class="table-section">
-              <h3 class="table-title">Vehicle Encyclopedia</h3>
-              <table class="score-table main-table">
-                <thead>
-                  <tr class="table-header">
-                    <th>Rank</th>
-                    <th>User</th>
-                    <th>Total Score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-            </div>
+  <div class="score-rank">
+    <h2 class="header">SCORE RANK</h2>
+
+    <!-- Filteri -->
+    <div class="filters">
+      <label class="filter-button">
+        <input type="radio" v-model="filterCategory" value="all" /> ALL
+      </label>
+      <label class="filter-button">
+        <input type="radio" v-model="filterCategory" value="military-library" /> MILITARY LIBRARY
+      </label>
+    </div>
+
+    <!-- Tablice -->
+    <div v-if="filterCategory === 'all'" class="table-section">
+      <h3 class="table-title">OVERALL SCORES</h3>
+      <table class="score-table overall-table">
+        <thead>
+          <tr class="table-header">
+            <th>RANK</th>
+            <th>USER</th>
+            <th>TOTAL SCORE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in overallScores" :key="user.name">
+            <td>{{ index + 1 }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.totalScore }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div v-else-if="filterCategory === 'military-library'" class="table-section">
+      <div class="table-pair">
+        <div class="table-container">
+          <h3 class="table-title">MILITARY ENCYCLOPEDIA LIBRARY</h3>
+          <table class="score-table main-table">
+            <thead>
+              <tr class="table-header">
+                <th>RANK</th>
+                <th>USER</th>
+                <th>TOTAL SCORE</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(user, index) in militaryEncyclopediaScores" :key="user.name">
+                <td>{{ index + 1 }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.totalScore }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div class="filters library-filters">
+            <label class="filter-button">
+              <input type="radio" v-model="libraryFilterCategory" value="weapons" /> WEAPONS ENCYCLOPEDIA
+            </label>
+            <label class="filter-button">
+              <input type="radio" v-model="libraryFilterCategory" value="vehicles" /> VEHICLE ENCYCLOPEDIA
+            </label>
           </div>
-  
-          <div class="table-container">
-            <h3 class="table-title">Military Essentials Library</h3>
+
+          <div v-if="libraryFilterCategory === 'weapons'" class="table-section">
+            <h3 class="table-title">WEAPONS ENCYCLOPEDIA</h3>
             <table class="score-table main-table">
               <thead>
                 <tr class="table-header">
-                  <th>Rank</th>
-                  <th>User</th>
-                  <th>Total Score</th>
+                  <th>RANK</th>
+                  <th>USER</th>
+                  <th>TOTAL SCORE</th>
                 </tr>
               </thead>
               <tbody>
+                <tr v-for="(user, index) in weaponsScores" :key="user.name">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.score }}</td>
+                </tr>
               </tbody>
             </table>
-  
-            <div class="filters library-filters">
-              <label>
-                <input type="radio" v-model="essentialsFilterCategory" value="equipment" /> Equipment Essentials
-              </label>
-              <label>
-                <input type="radio" v-model="essentialsFilterCategory" value="armory" /> Armory Essentials
-              </label>
-            </div>
-  
-            <div v-if="essentialsFilterCategory === 'equipment'" class="table-section">
-              <h3 class="table-title">Equipment Essentials</h3>
-              <table class="score-table main-table">
-                <thead>
-                  <tr class="table-header">
-                    <th>Rank</th>
-                    <th>User</th>
-                    <th>Total Score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-            </div>
-  
-            <div v-if="essentialsFilterCategory === 'armory'" class="table-section">
-              <h3 class="table-title">Armory Essentials</h3>
-              <table class="score-table main-table">
-                <thead>
-                  <tr class="table-header">
-                    <th>Rank</th>
-                    <th>User</th>
-                    <th>Total Score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-            </div>
+          </div>
+
+          <div v-if="libraryFilterCategory === 'vehicles'" class="table-section">
+            <h3 class="table-title">VEHICLE ENCYCLOPEDIA</h3>
+            <table class="score-table main-table">
+              <thead>
+                <tr class="table-header">
+                  <th>RANK</th>
+                  <th>USER</th>
+                  <th>TOTAL SCORE</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(user, index) in vehicleScores" :key="user.name">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.score }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div class="table-container">
+          <h3 class="table-title">MILITARY ESSENTIALS LIBRARY</h3>
+          <table class="score-table main-table">
+            <thead>
+              <tr class="table-header">
+                <th>RANK</th>
+                <th>USER</th>
+                <th>TOTAL SCORE</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(user, index) in militaryEssentialsScores" :key="user.name">
+                <td>{{ index + 1 }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.totalScore }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div class="filters library-filters">
+            <label class="filter-button">
+              <input type="radio" v-model="essentialsFilterCategory" value="equipment" /> EQUIPMENT ESSENTIALS
+            </label>
+            <label class="filter-button">
+              <input type="radio" v-model="essentialsFilterCategory" value="armory" /> ARMORY ESSENTIALS
+            </label>
+          </div>
+
+          <div v-if="essentialsFilterCategory === 'equipment'" class="table-section">
+            <h3 class="table-title">EQUIPMENT ESSENTIALS</h3>
+            <table class="score-table main-table">
+              <thead>
+                <tr class="table-header">
+                  <th>RANK</th>
+                  <th>USER</th>
+                  <th>TOTAL SCORE</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(user, index) in equipmentScores" :key="user.name">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.score }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div v-if="essentialsFilterCategory === 'armory'" class="table-section">
+            <h3 class="table-title">ARMORY ESSENTIALS</h3>
+            <table class="score-table main-table">
+              <thead>
+                <tr class="table-header">
+                  <th>RANK</th>
+                  <th>USER</th>
+                  <th>TOTAL SCORE</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(user, index) in armoryScores" :key="user.name">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.score }}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ScoreRank',
-    data() {
-      return {
-        filterCategory: 'all',
-        libraryFilterCategory: 'weapons',
-        essentialsFilterCategory: 'equipment',
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScoreRank',
+  data() {
+    return {
+      filterCategory: 'all',
+      libraryFilterCategory: 'weapons',
+      essentialsFilterCategory: 'equipment',
+      // Dummy data for testing
+      users: [
+        { name: 'User A', weapons: 500, vehicles: 400, equipment: 300, armory: 200 },
+        { name: 'User B', weapons: 450, vehicles: 400, equipment: 350, armory: 250 },
+        { name: 'User C', weapons: 400, vehicles: 450, equipment: 400, armory: 300 },
+      ]
+    };
+  },
+  computed: {
+    weaponsScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        score: user.weapons,
+      }));
     },
-  };
-  </script>
-  
-  <style scoped>
-  .score-rank {
-    font-family: 'Arial', sans-serif;
-    width: 80%;
-    margin: 0 auto;
-    padding: 20px;
+    vehicleScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        score: user.vehicles,
+      }));
+    },
+    militaryEncyclopediaScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        totalScore: user.weapons + user.vehicles,
+      }));
+    },
+    equipmentScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        score: user.equipment,
+      }));
+    },
+    armoryScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        score: user.armory,
+      }));
+    },
+    militaryEssentialsScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        totalScore: user.equipment + user.armory,
+      }));
+    },
+    overallScores() {
+      return this.users.map(user => ({
+        name: user.name,
+        totalScore: (user.weapons + user.vehicles) + (user.equipment + user.armory),
+      })).sort((a, b) => b.totalScore - a.totalScore);
+    }
   }
-  
-  .filters {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  .filters label {
-    margin-right: 15px;
-  }
-  
-  .library-filters {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  .table-section {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  .table-pair {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin-bottom: 20px;
-  }
-  
-  .table-container {
-    width: 48%; 
-    box-sizing: border-box; 
-    margin-bottom: 20px;
-  }
-  
-  .table-title {
-    margin-bottom: 10px;
-  }
-  
-  .score-table {
-    width: 100%;
-    border-collapse: collapse;
-    border-radius: 5px;
-    overflow: hidden;
-  }
-  
-  .table-header th {
-    background-color: #f2f2f2;
-    text-align: left;
-  }
-  
-  th, td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-  
-  td {
-    text-align: center;
-  }
-  
-  th:hover {
-    background-color: #e2e2e2;
-  }
-  
-  th.sorted {
-    font-weight: bold;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.score-rank {
+  font-family: 'mojFont', sans-serif;
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  color: #676767;
+}
+
+.filters {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.filter-button {
+  display: inline-block;
+  font-size: 1rem;
+  color: #898989;
+  text-decoration: none;
+  margin-right: 15px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: transparent;
+  border: none;
+  transition: color 0.3s;
+  cursor: pointer; 
+}
+
+.filter-button input[type="radio"] {
+  display: none;
+}
+
+.filter-button:hover,
+.filter-button input[type="radio"]:checked + .filter-button {
+  color: #676767;
+}
+
+.library-filters {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.table-section {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.table-pair {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+}
+
+.table-container {
+  width: 48%;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+}
+
+.table-title {
+  margin-bottom: 10px;
+  color: #00adb5;
+}
+
+.score-table {
+  width: 100%;
+  border-collapse: collapse;
+  overflow: hidden;
+}
+
+.overall-table {
+  width: 70%;
+  margin: 0 auto;
+}
+
+.table-header th {
+  background-color: #f2f2f2;
+  text-align: left;
+  color: #00adb5;
+}
+
+.table-header th, .score-table td {
+  color: #676767;
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+td {
+  text-align: center;
+}
+
+th:hover {
+  background-color: #e2e2e2;
+}
+
+th.sorted {
+  font-weight: bold;
+}
+</style>
